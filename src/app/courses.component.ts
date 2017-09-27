6c2626824fafe813f49a6416686715d7d1fb0c56
Aarish Saxena <aarish.work@gmail.com>
2017-09-27 22:44:04 +0530

    added CourseComponent

---
+++ src/app/courses.component.ts
@@ -0,0 +1,16 @@
+import { Component} from '@angular/core'
+
+@Component( 
+    {
+        selector: 'courses',
+                     // <courses> "courses"
+                     // < div id="courses">  ".courses"
+                     // < class id="courses">  "#courses" 
+
+        template: '<h3>There will be 3 courses</h3>'
+    }
+)
+
+export class CoursesComponent {
+
+}
